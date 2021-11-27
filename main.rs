661e74645e760bc48fa922157016078521535a11
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2021-11-27 18:18:16 +0700

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,105 @@
+// #[derive(Debug)]
+// struct Hinhchunhat{
+//     dai: u32,
+//     rong: u32,
+// }
+
+
+    // funtion
+    // let _s1 = String::from("World");
+    // let (_s2, len) = calculator_height(_s1);
+    // println!("{} {}",_s2, len);
+    // fn calculator_height(some_string: String) -> (String, usize){ // change to type data string -> string, value
+    //     let lenght = some_string.len();
+    //     (some_string, lenght)
+    // }
+    // let mut string = String::from("World");
+    // let (len) = calculator_height(&mut string);
+    // println!("{} {}", string, len);
+    // fn calculator_height(some_string: &mut std::string::String) -> (usize){ // change to type data string -> string, value
+    //     some_string.push_str("world");
+    //     let lenght = some_string.len();
+    //     lenght
+    //    println!("Today is {month} {day}, {year}", year=2017, month="September", day=28);
+
+// struct Hinhchunhat{
+//     dai: u32,
+//     rong: u32,
+// }
+
+fn main(){
+    // funtion
+    // let _s1 = String::from("World");
+    // let (_s2, len) = calculator_height(_s1);
+    // println!("{} {}",_s2, len);
+    // fn calculator_height(some_string: String) -> (String, usize){ // change to type data string -> string, value
+    //     let lenght = some_string.len();
+    //     (some_string, lenght)
+    // }
+    // let mut string = String::from("World");
+    // let (len) = calculator_height(&mut string);
+    // println!("{} {}", string, len);
+    // fn calculator_height(some_string: &mut std::string::String) -> (usize){ // change to type data string -> string, value
+    //     some_string.push_str("world");
+    //     let lenght = some_string.len();
+    //     lenght
+    //    println!("Today is {month} {day}, {year}", year=2017, month="September", day=28);
+
+    //     let mut member1 = Member{
+    //         id : 20126048,
+    //         email : String::from("thanhnhi@gmail.com"),
+    //         age : 19,
+    //         active: true,
+    //     };
+    //     let member3 = Member{
+    //             email : String::from("thanhnhi1011@gmail.com"),
+    //             ..member1
+    // };
+    //     println! ("member3: {:#?}",member3);
+    //     fn member2 (id: i8, email: String ) -> Member{
+    //         Member{
+    //             id: 2,
+    //             email: email,
+    //             age :  16,
+    //             active: true,
+
+    //         }
+    //     }
+    // let kichthuoc = Hinhchunhat{
+    //         dai: 16,
+    //         rong: 10,
+    // };
+    // println! ("dien tich: {:#?}",dientich(&kichthuoc));
+    // println! ("dai: {:#?}",&kichthuoc);
+
+    // fn dientich(kichthuoc: &Hinhchunhat) -> u32{
+    //     kichthuoc.dai*kichthuoc.rong
+    // fn hello() -> &'static str {
+    //     let s1 : &str = "Hello, World!" ;
+    //     s1
+    // }
+    
+    //     let s2 = hello();
+    //     println!("{}",s2);
+//    struct Rectangle{
+//         with: i32,
+//         height : i32,
+//    }
+//    let a = Rectangle{
+//         with : 32,
+//         height : 32,
+//    };
+
+//    println! ("dien tich: {:#?}",area(&a));
+
+//    fn area(rectangle: &Rectangle) -> i32{
+//         rectangle.height * rectangle.with
+//    }
+
+pub fn reverse(input: &str) -> String {
+    unimplemented!("Write a function to reverse {}", input);
+    
+}
+reverse("thanh nhi");new
+
+}
